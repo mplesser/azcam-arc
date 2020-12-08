f240db8852d79628cb129064a8e533b1db2ea93b
Michael Lesser <mlesser@arizona.edu>
2020-12-07 17:52:33 -0700

    cleanup

--- azcam_arc/arc_commands.py
+++ azcam_arc/arc_commands.py
@@ -27,13 +27,16 @@ def start_idle():
     return
 
 
-def set_bias_number(board_number, dac_number, board_type, dac_value):
+def set_bias_number(
+    board_number: int, dac_number: int, board_type: str, dac_value: int
+):
     """
     Sets a bias value.
-    BoardNumber is the controller board number.
-    DAC is DAC number.
-    Type is 'VID' or 'CLK'.
-    DacValue is DAC value for voltage.
+    Args:
+        board_number: controller board number
+        dac_number: DAC number
+        board_type: 'VID' or 'CLK'
+        dac_value: DAC value for voltage
     """
 
     s = f"controller.set_bias_number {board_number} {dac_number} {board_type} {dac_value}"
@@ -42,13 +45,14 @@ def set_bias_number(board_number, dac_number, board_type, dac_value):
     return
 
 
-def write_controller_memory(mem_type, board_number, address, value):
+def write_controller_memory(mem_type: str, board_number: int, address: int, value: int):
     """
     Write a word to a DSP memory location.
-    Type is P, X, Y, or R memory space.
-    BoardNumber is controller board number.
-    Address is memory address to write.
-    value is data to write.
+    Args:
+        mem_type: P, X, Y, or R memory space
+        board_number: controller board number
+        address: memory address to write
+        value: data to write
     """
 
     s = f"controller.write_memory {mem_type} {board_number} {address} {value}"
@@ -57,12 +61,15 @@ def write_controller_memory(mem_type, board_number, address, value):
     return
 
 
-def read_controller_memory(mem_type, board_number, address):
+def read_controller_memory(mem_type: str, board_number: int, address: int):
     """
     Read from DSP memory.
-    Type is P, X, Y, or R memory space.
-    BoardNumber is controller board number.
-    Address is memory address to read.
+    Args:
+        mem_type: P, X, Y, or R memory space
+        board_number: controller board number
+        address: memory address to read
+    Returns:
+        Value of memory
     """
 
     s = f"controller.read_memory {mem_type} {board_number} {address}"
@@ -75,9 +82,10 @@ def board_command(command, board_number, arg1=-1, arg2=-1, arg3=-1, arg4=-1):
     """
     Send a specific command to an ARC controller board.
     The reply from the board is not usually 'OK', it is often 'DON' but could be data.
-    Command is the board command to send.
-    BoardNumber is controller board number.
-    ArgN are arguments for Cmd.
+    Args:
+        command: board command to send
+        board_number: controller board number
+        argN: arguments for command
     """
 
     s = f"controller.board_command {command} {board_number} {arg1} {arg2} {arg3} {arg4}"
